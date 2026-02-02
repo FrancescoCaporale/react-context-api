0950f99ef29879cffea799ef44982f64357ddd90
FrancescoCaporale <frenk.caporale@gmail.com>
2026-02-02 16:35:57 +0100

    Aggiunto Budgetprovider e BudgetState

---
+++ src/context/BudgetContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+const BudgetContext = createContext();
+
+function BudgetProvider({ children }) {
+    const [budgetMode, setBudgetMode] = useState(false);
+
+    return (
+        <BudgetContext.Provider
+            value={{
+                budgetMode,
+                setBudgetMode,
+            }}
+        >
+            {children}
+        </BudgetContext.Provider>
+    );
+}
+
+export { BudgetProvider };
